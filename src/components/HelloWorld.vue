d123b7e35272d8d22db09c57811742888cb336aa
18李贵洪 <1551887484@qq.com>
2024-06-22 11:28:39 +0800

    修改文本

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -8,7 +8,7 @@ defineProps({
 const count = ref(0)
 </script>
 <template>
- <h1>Privacy Policy</h1>
+ <h1>PixelBoard Privacy Policy</h1>
   <p>Last updated: June 17, 2024</p>
    <p>This Privacy Policy describes Our policies and procedures on the collection, use and disclosure of Your information when You use the Service and tells You about Your privacy rights and how the law protects You.</p>
    <p>We use Your Personal data to provide and improve the Service. By using the Service, You agree to the collection and use of information in accordance with this Privacy Policy.</p> 
