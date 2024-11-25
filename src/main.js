9ebadf09b9c3504e61d5be134c23da424bc64b2f
18李贵洪 <1551887484@qq.com>
2024-11-25 16:10:35 +0800

    easychart

--- src/main.js
+++ src/main.js
@@ -1,5 +1,16 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import EasyChart from './components/EasyChart.vue';
 
-createApp(App).mount('#app')
+const app = document.getElementById('app');
+
+if (app) {
+  const pageType = app.getAttribute('data-page'); // 获取自定义属性
+
+  if (pageType === 'easychart') {
+    createApp(EasyChart).mount('#app');
+  } else {
+    createApp(App).mount('#app');
+  }
+}
